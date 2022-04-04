a4c52238c9abc6afd3c213410800bd555eed38e4
Ahren Gann <algbbg@gmail.com>
2022-04-04 09:59:33 -0500

    Fill Unit Table SQL added

---
+++ Database/FillMeasurementUnitTable.sql
@@ -0,0 +1,15 @@
+INSERT INTO Data.MeasurementUnit(Name)
+VALUES
+    ('whole'),
+    ('cup'),
+    ('tablespoon'),
+    ('teaspoon'),
+    ('gallon'),
+    ('quart'),
+    ('ounce'),
+    ('pint'),
+    ('half'),
+    ('dash'),
+    ('gram'),
+    ('kilogram'),
+    ('fluid ounce');
