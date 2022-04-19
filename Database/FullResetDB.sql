06207f6d8cfb2e12b49fd70ec9ec9b0e2077ebd8
Ahren Gann <algbbg@gmail.com>
2022-04-18 22:15:36 -0500

    Update Ingredient Parameters

--- Database/FullResetDB.sql
+++ Database/FullResetDB.sql
@@ -34,7 +34,7 @@ GO
 
 CREATE TABLE Data.Ingredient(
     IngredientID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    [Name] NVARCHAR(32) NOT NULL UNIQUE,
+    [Name] NVARCHAR(128) NOT NULL UNIQUE,
     IsRemoved bit default 0 NOT NULL,
     CreatedOn datetimeoffset default SYSDATETIMEOFFSET() NOT NULL,
     ModifiedOn datetimeoffset,
