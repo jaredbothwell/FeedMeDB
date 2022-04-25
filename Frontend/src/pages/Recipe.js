b663b56689587b5b2e7a5c4c769077e9768742c2
kevin duong <Kevduong@ksu.edu>
2022-04-25 17:27:01 -0500

    Filter ingredients and Recipes pages (WIP)

--- Frontend/src/pages/Recipe.js
+++ Frontend/src/pages/Recipe.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AnimatedPage from './AnimatedPage';
-
+import single_recipe from '../mock_data/single_recipe';
+import './css_files/recipe.css'
 export default function Recipe() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const recipeId = urlParams.get('recipeId')
-    const recipeName = urlParams.get('name')
+
+  const [recipe,setRecipe] = useState(single_recipe);
+
 
   return (
     <AnimatedPage>
-    <div style={{color:"white", fontSize: 20, textAlign: "center"}}>recipe Name: {recipeName}</div>
-    <div style={{color:"white", fontSize: 20, textAlign: "center"}}>recipe Id: {recipeId}</div>
+      
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+          <div>
+            <h1 style={{color:'wheat'}} >{recipe.name}</h1>
+            <div className='form_container' >
+              <div className='display_label' >{recipe.name}</div>
+              <div className='display_label' >{recipe.prepTime}</div>
+              <div className='display_label' >{recipe.difficulty}</div>
+              <div className='display_label' >{recipe.directions}</div>
+            </div>
+          </div>
+        </div>
     </AnimatedPage>
   )
 }
