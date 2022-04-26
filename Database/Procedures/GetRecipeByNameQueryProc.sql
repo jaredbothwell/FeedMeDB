0d8022b4006bae7bcaaf3839fa481032449765ed
Ahren Gann <algbbg@gmail.com>
2022-04-25 22:48:00 -0500

    Updated GetRecipeByNameQuery

--- Database/Procedures/GetRecipeByNameQueryProc.sql
+++ Database/Procedures/GetRecipeByNameQueryProc.sql
@@ -1,7 +1,7 @@
 CREATE OR ALTER PROCEDURE Data.GetRecipeByNameQuery
 @UserQuery NVARCHAR(256)
 AS
-SELECT DISTINCT(R.RecipeID),R.Name,R.CreatedUserID,R.Difficulty,R.Directions,R.PrepTime,R.RemovedOn
+SELECT *
 FROM Data.Recipe R
 WHERE R.Name LIKE '%' + @UserQuery + '%'
 
