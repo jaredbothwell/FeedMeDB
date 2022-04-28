f9aa0bf5c8c8ed2e917f7ee34f030f41bcd749a5
Kevin <kevduong@ksu.edu>
2022-04-27 23:46:16 -0500

    cleaned up 'my recipes'

--- Frontend/src/components/AccountMenu.jsx
+++ Frontend/src/components/AccountMenu.jsx
@@ -15,17 +15,15 @@ export default function AccountMenu() {
     setAnchorEl(null);
   };
 
-  const [myRecipes,enableMyrecipes] = useState(false);
+  const [myRecipes,setMyRecipes] = useState(false);
   const [savedRecipes,enableSaved] = useState(false);
-
-
   return (
       
     <div>
         <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={true}>
-          <MyRecipes/>
+        open={myRecipes}>
+          <MyRecipes closeForm={()=>{setMyRecipes(false)}}/>
         </Backdrop>
 
       <IconButton
@@ -47,7 +45,7 @@ export default function AccountMenu() {
         }}
       >
 
-        <MenuItem onClick={handleClose}>My Recipes</MenuItem>
+        <MenuItem onClick={()=>{setMyRecipes(true);handleClose();}}>My Recipes</MenuItem>
         <MenuItem onClick={handleClose}>BookMarked Recipes</MenuItem>
         <MenuItem onClick={()=>{
           localStorage.setItem('user_id',null);
--- Frontend/src/components/MyRecipes.jsx
+++ Frontend/src/components/MyRecipes.jsx
@@ -1,16 +1,105 @@
-import React, { useEffect, useState } from 'react'
 import './css_files/Ingredient.css'
-import RecipesTable from './RecipesTable';
+import React, { useEffect, useState } from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { Backdrop, Fab, IconButton } from '@mui/material';
+import CreateRecipeForm from './CreateRecipeForm';
 
 
-export default function MyRecipes() {
+export default function MyRecipes({closeForm}) {
+
+  const [recipes, setRecipes] = useState([]);
+
+  const [backDropOpen, setBackDrop] = useState(false);
+  const [uniqueKey,incrementKey] = useState(0);
+
+  useEffect(()=>
+  {
+    const user_id = localStorage.getItem('user_id');
+    if(user_id != null)
+    {
+      fetch(
+        "http://localhost:8000/api/recipes/createdby/" + user_id)
+        .then((res) => res.json())
+        .then((json) => {
+          setRecipes(json);
+          console.log(json)
+        })
+    }
+  },[uniqueKey])
 
   return (
     <div className='form_container1'>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backDropOpen}
+        
+      >
+        <CreateRecipeForm key={uniqueKey} closeHandler={()=>{setBackDrop(false); incrementKey(uniqueKey+1);}}/>
+      </Backdrop>
         <h1>
             My Recipes
         </h1>
-        <RecipesTable/>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Recipe</TableCell>
+            <TableCell align="center">Created On</TableCell>
+            <TableCell align="right">Edit</TableCell>
+            <TableCell align="right">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {recipes.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.createdOn.substring(0,10)}</TableCell>
+              <TableCell align="right">
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+
+                        >
+                        <EditIcon/>
+                    </IconButton>
+              </TableCell>
+              <TableCell align="right">
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+
+                        >
+                        <DeleteIcon/>
+                    </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <div className='input_field'>
+    <Fab onClick={()=>setBackDrop(!backDropOpen)} variant="extended" color="primary" aria-label="add">
+                Add Recipe
+    </Fab>    
+    <Fab sx={{width:120, backgroundColor: 'gray', marginLeft:2}} onClick={closeForm} variant="extended" color="primary" aria-label="add">
+                Close
+        </Fab>
+
+    </div>
+
     </div>
   )
 }
