4339a2ea4b30885114afaca076cd53ceae70e8ed
Jared Bothwell <jaredcbothwell@gmail.com>
2022-05-01 20:10:58 -0500

    create update UserRecipes

--- Database/Procedures/CreateUser.sql
+++ Database/Procedures/CreateUser.sql
@@ -1,16 +1,16 @@
 CREATE OR ALTER PROCEDURE Data.CreateUser
-@username NVARCHAR(20),
+@userName NVARCHAR(20),
 @passwordHash NVARCHAR(256)
 AS
 
 BEGIN
 
 INSERT INTO Data.[User] (UserName, PasswordHash) 
-    VALUES (@username, @passwordHash)
+    VALUES (@userName, @passwordHash)
 
 SELECT U.UserID, U.UserName, U.PasswordHash, U.CreatedOn, U.ModifiedOn, U.RemovedOn 
 FROM Data.[User] U 
-WHERE U.UserName = @username
+WHERE U.UserName = @userName
     AND U.RemovedOn IS NULL
 
 END
