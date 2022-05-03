1c4277e776751a5145c2e80338805197a92a4c7b
Kevin <kevduong@ksu.edu>
2022-05-02 20:02:41 -0500

    remove feature

--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -1,155 +1,195 @@
-import React from 'react'
-import './css_files/LoginForm.css'
+import React from "react";
+import "./css_files/LoginForm.css";
 
 export class JawbreakerInput extends React.Component {
-    constructor() {
-      super();
-      this.handleChange = this.handleChange.bind(this);
-      this.handleBlur = this.handleBlur.bind(this);
-      this.handleFocus = this.handleFocus.bind(this);
-    }
-    handleChange(event) {
-      this.props.Change(this.props.ID, event.target.value);
-    }
-    handleFocus(event) {
-      this.props.Focus(this.props.ID);
-    }
-    handleBlur(event) {
-      this.props.Blur(this.props.ID);
-    }
-      render() {
-          return (
-        <span className={(this.props.Value.length > 0 || this.props.Focused) ? 'input filled' : 'input'}>
-          <input value={this.props.Value} onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleChange} className="field" type={this.props.Type} />
-          <label className={(this.props.Error) ? 'label error' : 'label'}>
-            <span className="content">{this.props.Placeholder}</span>
-          </label>
-          <span className={(this.props.Error) ? 'errorMessage visible' : 'errorMessage'}>This field cannot be empty.</span>
-        </span>
-      );
-      }
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
-  
-  export class LoginForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        submitting: false,
-        error: false,
-        inputs: [
-          {
-            focused: false,
-            value: '',
-            error: false
-          },
-          {
-            focused: false,
-            value: '',
-            error: false
-          }
-        ]
-      };
+  handleChange(event) {
+    this.props.Change(this.props.ID, event.target.value);
+  }
+  handleFocus(event) {
+    this.props.Focus(this.props.ID);
+  }
+  handleBlur(event) {
+    this.props.Blur(this.props.ID);
+  }
+  render() {
+    return (
+      <span
+        className={
+          this.props.Value.length > 0 || this.props.Focused
+            ? "input filled"
+            : "input"
+        }
+      >
+        <input
+          value={this.props.Value}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+          className="field"
+          type={this.props.Type}
+        />
+        <label className={this.props.Error ? "label error" : "label"}>
+          <span className="content">{this.props.Placeholder}</span>
+        </label>
+        <span
+          className={this.props.Error ? "errorMessage visible" : "errorMessage"}
+        >
+          This field cannot be empty.
+        </span>
+      </span>
+    );
+  }
+}
 
-
-
-      this.verifyCreds = this.verifyCreds.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleFocus = this.handleFocus.bind(this);
-      this.handleBlur = this.handleBlur.bind(this);
-    }
-
-    verifyCreds(user,pass)
-    {
-      // need to add checks for if user exists already, and if two passwords match
-      let http_string = "http://localhost:8000/api/users/login?userName=" + user +"&password=" + pass 
-      console.log(http_string)
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: 'React POST Request Example' })
+export class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitting: false,
+      error: false,
+      inputs: [
+        {
+          focused: false,
+          value: "",
+          error: false,
+        },
+        {
+          focused: false,
+          value: "",
+          error: false,
+        },
+      ],
     };
 
-
-
-    fetch(http_string, requestOptions)
-        .then(response => response.json().then((u)=>{
-          if (u != null)
-          {
-            this.props.updateStatus([u,true])
-          }
-          else
-          {
-            this.props.updateStatus([null,false])
-          }
-          
-        }))
-
-      // add some fail code here
-    }
-    
-
-    handleChange(id, value) {
-      // immutable array
-      let newInputs = this.state.inputs.concat();
-      newInputs[id].value = value;
-      newInputs[id].error = false;
-      this.setState({inputs: newInputs});
-    }
-    handleSubmit(event) {
-      let errors = false;
-      let newInputs = this.state.inputs.concat();
-      for(let i = 0; i < this.state.inputs.length; i++) {
-        if (this.state.inputs[i].value.length === 0) {
-          errors = true;
-          newInputs[i].error = true;
-        }
-      }
-      
-      this.setState({
-        error: errors,
-        inputs: newInputs
-      });
-      
-      if (!errors) {
-        this.setState({submitting: true});
-        setTimeout(() => {
-          let newInputs = this.state.inputs.concat();
-          for(let i = 0; i < this.state.inputs.length; i++) {
-            newInputs[i].value = '';
-          }
-  
-          this.setState({
-            submitting: false,
-            inputs: newInputs
-          });
-        }, 2000);
-      }
-      
-      event.preventDefault();
-      this.verifyCreds(this.state.inputs[0].value, this.state.inputs[1].value);
-    }
-    handleFocus(id) {
-      // immutable array
-      let newInputs = this.state.inputs.concat();
-      newInputs[id].focused = true;
-      this.setState({inputs: newInputs});
-    }
-    handleBlur(id) {
-      // immutable array
-      let newInputs = this.state.inputs.concat();
-      newInputs[id].focused = false;
-      this.setState({inputs: newInputs});
-    }
-    render() {
-      return(
-        <form onSubmit={this.handleSubmit}>
-          <JawbreakerInput ID="0" Placeholder="Username" Type="text" Focus={this.handleFocus} Blur={this.handleBlur} Change={this.handleChange} Value={this.state.inputs[0].value} Focused={this.state.inputs[0].focused} Error={this.state.inputs[0].error} />
-          <JawbreakerInput ID="1" Placeholder="Password" Type="password" Focus={this.handleFocus} Blur={this.handleBlur} Change={this.handleChange} Value={this.state.inputs[1].value} Focused={this.state.inputs[1].focused} Error={this.state.inputs[1].error} />
-          <button type="submit" className={(this.state.inputs[1].error || this.state.inputs[0].error) ? 'iconButton error' : 'iconButton'} disabled={this.state.submitting}>
-            <span className="label">Login</span>
-          </button>
-        </form>
-      )
-    }
+    this.verifyCreds = this.verifyCreds.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
+
+  verifyCreds(user, pass) {
+    // need to add checks for if user exists already, and if two passwords match
+    let http_string =
+      "http://localhost:8000/api/users/login?userName=" +
+      user +
+      "&password=" +
+      pass;
+    console.log(http_string);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "React POST Request Example" }),
+    };
+
+    fetch(http_string, requestOptions).then((response) =>
+      response.json().then((u) => {
+        if (u != null) {
+          this.props.updateStatus([u, true]);
+        } else {
+          this.props.updateStatus([null, false]);
+        }
+      })
+    );
+
+    // add some fail code here
+  }
+
+  handleChange(id, value) {
+    // immutable array
+    let newInputs = this.state.inputs.concat();
+    newInputs[id].value = value;
+    newInputs[id].error = false;
+    this.setState({ inputs: newInputs });
+  }
+  handleSubmit(event) {
+    let errors = false;
+    let newInputs = this.state.inputs.concat();
+    for (let i = 0; i < this.state.inputs.length; i++) {
+      if (this.state.inputs[i].value.length === 0) {
+        errors = true;
+        newInputs[i].error = true;
+      }
+    }
+
+    this.setState({
+      error: errors,
+      inputs: newInputs,
+    });
+
+    if (!errors) {
+      this.setState({ submitting: true });
+      setTimeout(() => {
+        let newInputs = this.state.inputs.concat();
+        for (let i = 0; i < this.state.inputs.length; i++) {
+          newInputs[i].value = "";
+        }
+
+        this.setState({
+          submitting: false,
+          inputs: newInputs,
+        });
+      }, 2000);
+    }
+
+    event.preventDefault();
+    this.verifyCreds(this.state.inputs[0].value, this.state.inputs[1].value);
+  }
+  handleFocus(id) {
+    // immutable array
+    let newInputs = this.state.inputs.concat();
+    newInputs[id].focused = true;
+    this.setState({ inputs: newInputs });
+  }
+  handleBlur(id) {
+    // immutable array
+    let newInputs = this.state.inputs.concat();
+    newInputs[id].focused = false;
+    this.setState({ inputs: newInputs });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <JawbreakerInput
+          ID="0"
+          Placeholder="Username"
+          Type="text"
+          Focus={this.handleFocus}
+          Blur={this.handleBlur}
+          Change={this.handleChange}
+          Value={this.state.inputs[0].value}
+          Focused={this.state.inputs[0].focused}
+          Error={this.state.inputs[0].error}
+        />
+        <JawbreakerInput
+          ID="1"
+          Placeholder="Password"
+          Type="password"
+          Focus={this.handleFocus}
+          Blur={this.handleBlur}
+          Change={this.handleChange}
+          Value={this.state.inputs[1].value}
+          Focused={this.state.inputs[1].focused}
+          Error={this.state.inputs[1].error}
+        />
+        <button
+          type="submit"
+          className={
+            this.state.inputs[1].error || this.state.inputs[0].error
+              ? "iconButton error"
+              : "iconButton"
+          }
+          disabled={this.state.submitting}
+        >
+          <span className="label">Login</span>
+        </button>
+      </form>
+    );
+  }
+}
--- Frontend/src/components/MyRecipes.jsx
+++ Frontend/src/components/MyRecipes.jsx
@@ -1,114 +1,145 @@
-import './css_files/Ingredient.css'
-import React, { useEffect, useState } from 'react'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import { Backdrop, Fab, IconButton, Typography } from '@mui/material';
-import CreateRecipeForm from './CreateRecipeForm';
-import EditRecipeForm from './EditRecipeForm';
+import "./css_files/Ingredient.css";
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Backdrop, Fab, IconButton, Typography } from "@mui/material";
+import CreateRecipeForm from "./CreateRecipeForm";
+import EditRecipeForm from "./EditRecipeForm";
 
-
-
-export default function MyRecipes({closeForm}) {
-
+export default function MyRecipes({ closeForm }) {
   const [recipes, setRecipes] = useState([]);
   const [recipeToEdit, setRecipeToEdit] = useState(null);
   const [createRecipeBackdrop, setCreateRecipeBackdrop] = useState(false);
-  const [uniqueKey,incrementKey] = useState(0);
+  const [uniqueKey, incrementKey] = useState(0);
 
-  useEffect(()=>
-  {
-    const user_id = localStorage.getItem('user_id');
-    if(user_id != null)
-    {
-      fetch(
-        "http://localhost:8000/api/recipes/createdby/" + user_id)
+  useEffect(() => {
+    const user_id = localStorage.getItem("user_id");
+    if (user_id != null) {
+      fetch("http://localhost:8000/api/recipes/createdby/" + user_id)
         .then((res) => res.json())
         .then((json) => {
           let temp = json;
           setRecipes(temp);
-        })
+        });
     }
-  },[uniqueKey,recipeToEdit])
+  }, [uniqueKey, recipeToEdit]);
 
+  const deleteRecipe = (id) => {
+    console.log(id);
 
+    let http_string = "http://localhost:8000/api/recipes/remove?id=" + id;
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "React POST Request Example" }),
+    };
+    fetch(http_string, requestOptions).then((response) =>
+      console.log(response)
+    );
+    incrementKey(uniqueKey + 1);
+  };
 
   return (
-    <div className='form_container1'>
+    <div className="form_container1">
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={createRecipeBackdrop}
-        
       >
-        <CreateRecipeForm key={uniqueKey} closeHandler={()=>{setCreateRecipeBackdrop(false); incrementKey(uniqueKey+1);}}/>
+        <CreateRecipeForm
+          key={uniqueKey}
+          closeHandler={() => {
+            setCreateRecipeBackdrop(false);
+            incrementKey(uniqueKey + 1);
+          }}
+        />
       </Backdrop>
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={recipeToEdit !== null}>
-          <EditRecipeForm key={uniqueKey} closeHandler={()=>{setRecipeToEdit(null); incrementKey(uniqueKey+1);}} recipeData={recipeToEdit}/>
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={recipeToEdit !== null}
+      >
+        <EditRecipeForm
+          key={uniqueKey}
+          closeHandler={() => {
+            setRecipeToEdit(null);
+            incrementKey(uniqueKey + 1);
+          }}
+          recipeData={recipeToEdit}
+        />
       </Backdrop>
-      <Typography gutterBottom variant="h4" className='Display_Recipe_Text'>
-            My Recipes
-        </Typography>
-        <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Recipe</TableCell>
-            <TableCell align="center">Created On</TableCell>
-            <TableCell align="right">Edit</TableCell>
-            <TableCell align="right">Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {recipes.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.createdOn.substring(0,10)}</TableCell>
-              <TableCell align="right">
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={()=>setRecipeToEdit(row)}
-                        >
-                        <EditIcon/>
-                    </IconButton>
-              </TableCell>
-              <TableCell align="right">
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-
-                        >
-                        <DeleteIcon/>
-                    </IconButton>
-              </TableCell>
+      <Typography gutterBottom variant="h4" className="Display_Recipe_Text">
+        My Recipes
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Recipe</TableCell>
+              <TableCell align="center">Created On</TableCell>
+              <TableCell align="right">Edit</TableCell>
+              <TableCell align="right">Delete</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <div className='input_field'>
-    <Fab onClick={()=>setCreateRecipeBackdrop(!createRecipeBackdrop)} variant="extended" color="primary" aria-label="add">
-                Add Recipe
-    </Fab>    
-    <Fab sx={{width:120, backgroundColor: 'gray', marginLeft:2}} onClick={closeForm} variant="extended" color="primary" aria-label="add">
-                Close
+          </TableHead>
+          <TableBody>
+            {recipes.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">
+                  {row.createdOn.substring(0, 10)}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => setRecipeToEdit(row)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => deleteRecipe(row.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className="input_field">
+        <Fab
+          onClick={() => setCreateRecipeBackdrop(!createRecipeBackdrop)}
+          variant="extended"
+          color="primary"
+          aria-label="add"
+        >
+          Add Recipe
         </Fab>
-
+        <Fab
+          sx={{ width: 120, backgroundColor: "gray", marginLeft: 2 }}
+          onClick={closeForm}
+          variant="extended"
+          color="primary"
+          aria-label="add"
+        >
+          Close
+        </Fab>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
